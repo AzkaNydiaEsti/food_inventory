28d34dbaf0a00f63588beacbd4fe4cb7a5ada73a
AzkaNydiaEsti <azka.nydia@ui.ac.id>
2023-09-12 00:15:19 +0700

    update isi tabel

--- main/views.py
+++ main/views.py
@@ -5,13 +5,11 @@ def show_main(request):
     context = {
         'name': 'Azka Nydia Estiningtyas',
         'class': 'PBP E',
-        'item name': 'Adeptus Temptation',
-        'item quality': '5-Star',
-        'item type': 'ATK-Boosting Dishes',
-        'item description': 'Increases all party members ATK by 260~372 and CRIT Rate by 8~12% for 300s.',
-        'item amount': '2'
-   
-        
+        'item_name': 'Adeptus Temptation',
+        'quality': '5-Star',
+        'type': 'ATK-Boosting Dishes',
+        'description': 'Increases all party members ATK by 260~372 and CRIT Rate by 8~12 for 300s.',
+        'amount': '2'
     }
 
     return render(request, "main.html", context)
