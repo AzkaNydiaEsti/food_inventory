be27028b8d174b2531a299c70e4536fa4711b6be
AzkaNydiaEsti <azka.nydia@ui.ac.id>
2023-09-12 11:22:17 +0700

    revisi header dan models

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class Items(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
